f472c8bfb25fcaa34e3808d44278266daae8f44e
vmu-dev <58797755+vmu-dev@users.noreply.github.com>
2020-02-26 21:47:27 +0530

    Add files via upload

---
+++ Capstone-Project-w1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook for the capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
